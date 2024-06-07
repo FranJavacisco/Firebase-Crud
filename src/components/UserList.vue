b2f330465ade55f26fc1bcdfcb0462ae2637b8b4
Francisco Lopez <lopezf28@gmail.com>
2024-06-06 23:42:51 -0400

    creando componente

---
+++ src/components/UserList.vue
@@ -0,0 +1,30 @@
+<!-- src/components/UserList.vue -->
+<template>
+    <div>
+        <h2>Lista de Usuarios</h2>
+        <ul>
+            <li v-for="user in users" :key="user.id">
+                {{ user.name }} - {{ user.email }}
+                <button @click="deleteUser(user.id)">Eliminar</button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        users() {
+            return this.$store.state.users;
+        }
+    },
+    methods: {
+        deleteUser(id) {
+            this.$store.dispatch('deleteUser', id);
+        }
+    },
+    created() {
+        this.$store.dispatch('fetchUsers');
+    }
+};
+</script>
