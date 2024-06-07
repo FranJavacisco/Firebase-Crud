5cc99afabe94c64f6c8f2d3162cdca11b9f81cc4
Francisco Lopez <lopezf28@gmail.com>
2024-06-06 23:40:05 -0400

    creando componentes

---
+++ src/components/AddUser.vue
@@ -0,0 +1,33 @@
+<!-- src/components/AddUser.vue -->
+<template>
+    <div>
+        <h2>Agregar Usuario</h2>
+        <form @submit.prevent="submitForm">
+            <input v-model="name" type="text" placeholder="Nombre" required>
+            <input v-model="email" type="email" placeholder="Correo electrónico" required>
+            <button type="submit">Agregar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: '',
+            email: ''
+        };
+    },
+    methods: {
+        submitForm() {
+            const user = {
+                name: this.name,
+                email: this.email
+            };
+            this.$store.dispatch('addUser', user);
+            this.name = '';
+            this.email = '';
+        }
+    }
+};
+</script>
